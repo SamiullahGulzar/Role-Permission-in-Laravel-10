c09219bd6cabc21cb331ea3359dfb3431da8d985
SamiullahGulzar <samigulzar178@gmail.com>
2023-10-13 07:04:53 +0430

    Role created for admin, writer and user

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// custom routes
+Route::get('/admin', function(){
+    return view('admin.index');
+})->middleware(['auth', 'role:admin'])->name('admin.index');
+// end custom routes
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
